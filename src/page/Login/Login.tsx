3ee8c188bb186219c630d2e16d62e43d8ea2b7b1
edward <edwardbrian96@gmail.com>
2022-09-30 22:29:40 -0500

    vercel

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -47,7 +47,7 @@ export const Login = () => {
     <ContainerLogin>
       <BsPersonFill size="40" />
       <h3>Welcome user !</h3>
-      {process.env.REACT_APP_API_URL}
+      {process.env.REACT_APP_VERCEL_ENV}
       <Form>
         <Input
           placeholder="add email"
